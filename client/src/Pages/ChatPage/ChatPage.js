9e60896393d60d0c93a803f94ea3d5db9062b670
Artem Pavlenko <artempm18@gmail.com>
2026-05-28 15:33:59 +0300

    Fix: change hardcoded localhost URL to API_URL in ChatPage form submit

--- client/src/Pages/ChatPage/ChatPage.js
+++ client/src/Pages/ChatPage/ChatPage.js
@@ -103,7 +103,7 @@ const ChatPage = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8000/api/chat', {
+      const response = await fetch(`${API_URL}/api/chat`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
